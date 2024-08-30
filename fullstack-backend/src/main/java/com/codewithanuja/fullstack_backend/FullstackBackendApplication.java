2f10b3a0f176b33fc625e105e1b371470106b15e
Anuja Ingle <anujamanikrao@gmail.com>
2024-08-30 12:33:56 +0530

    Full Stack Backend

---
+++ fullstack-backend/src/main/java/com/codewithanuja/fullstack_backend/FullstackBackendApplication.java
@@ -0,0 +1,15 @@
+package com.codewithanuja.fullstack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* created by - Anuja Ingle */
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+}
